749f8561ad54d4fe800506d75039886726d106b5
T-ara32 <x04.magic.z5@gmail.com>
2022-07-23 15:19:28 +0900

    shopCreateDelete

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Owner; //エロくアント
+use App\Models\Owner; //エロクアント
+use App\Models\Shop; //エロクアント
 use Illuminate\Support\Facades\DB; //クエリビルダ
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+use Throwable;
 
 class OwnersController extends Controller
 {
@@ -26,7 +29,7 @@ class OwnersController extends Controller
     {
        $owners = Owner::select('id', 'name', 'email', 'created_at')
        ->paginate(3);
-       
+
         return view('admin.owners.index', compact('owners'));
     }
 
@@ -54,12 +57,27 @@ class OwnersController extends Controller
             'password' => 'required|string|confirmed|min:8',
         ]);
 
-        Owner::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
-        
+        try{
+            DB::transaction(function () use($request) {
+                $owner = Owner::create([
+                    'name' => $request->name,
+                    'email' => $request->email,
+                    'password' => Hash::make($request->password),
+                ]);
+
+                Shop::create([
+                    'owner_id' => $owner->id,
+                    'name' => '店名を入力してください',
+                    'information' => '',
+                    'filename' => '',
+                    'is_selling' => true
+                ]);
+            }, 2);
+        }catch(Throwable $e) {
+            Log::error($e);
+            throw $e;
+        }
+
         return redirect()
         ->route('admin.owners.index')
         ->with(['message' => 'オーナー登録を実施しました',
