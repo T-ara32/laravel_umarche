180073a04da19831f9413124b398e3f6d3f86c21
T-ara32 <x04.magic.z5@gmail.com>
2022-07-27 00:41:29 +0900

    cart index

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -6,9 +6,22 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Cart;
+use App\Models\User;
 
 class CartController extends Controller
 {
+    public function index() {
+        $user = User::findOrFail(Auth::id());
+        $products = $user->products;
+        $totalPrice = 0;
+
+        foreach($products as $product) {
+            $totalPrice += $product->price * $product->pivot->quantity;
+        }
+
+        return view('user.cart', compact('products', 'totalPrice'));
+    }
+
     public function add(Request $request) {
         $itemInCart = Cart::where('product_id', $request->product_id)
         ->where('user_id', Auth::id())->first();
@@ -23,6 +36,7 @@ class CartController extends Controller
                 'quantity' => $request->quantity
             ]);
         }
-        dd('テスト');
+
+        return redirect()->route('user.cart.index');
     }
 }
