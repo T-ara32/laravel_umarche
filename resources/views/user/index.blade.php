8660cccc717197d7e9fb9d58947ee28fd25208d1
T-ara32 <x04.magic.z5@gmail.com>
2022-07-27 15:11:04 +0900

    表示件数

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -30,7 +30,20 @@
                   </option>
                 </select>
               </div>
-              <div>表示件数</div>
+              <div>
+                <span class="text-sm">表示件数</span><br>
+                <select id="sort" name="sort" class="mr-4">
+                  <option value="20" @if(\Request::get('pagination') === '20') selected @endif>
+                    20件
+                  </option>
+                  <option value="50" @if(\Request::get('pagination') === '50') selected @endif>
+                    50件
+                  </option>
+                  <option value="100" @if(\Request::get('pagination') === '100') selected @endif>
+                    100件
+                  </option>
+                </select>
+              </div>
             </div>
           </form>
         </div>
@@ -57,6 +70,10 @@
                           </div>
                         @endforeach
                     </div>
+                    {{ $products->appends([
+                      'sort' => \Request::get('sort'),
+                      'pagination' => \Request::get('pagination')
+                    ])->links() }}
                 </div>
             </div>
         </div>
@@ -64,6 +81,11 @@
 <script>
   const select = document.getElementById('sort')
   select.addEventListener('change', function() {
+    this.form.submit()
+  })
+
+  const paginate = document.getElementById('pagination')
+  paginate.addEventListener('change', function() {
     this.form.submit()
   })
 </script>
